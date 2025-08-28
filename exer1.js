91ea80243bc133c5e75d74ff93bf61276cbadae9
LeonardoGrassiZambrin <leonardo.zambrin@escola.pr.gov.br>
2025-08-28 12:42:20 -0300

    Add files via upload

---
+++ exer1.js
@@ -0,0 +1,9 @@
+// Bem vindo ao meu programa JavaScript !!
+
+console.log("Meu Primeito Programa JS");
+
+const idadeleo = 17;
+const idadefelipe = 16;
+
+console.log("A idade do Leonardo é " + idadeleo);
+console.log("A idade do Felipe é " + idadefelipe);
