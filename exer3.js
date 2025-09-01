1b88a8ca8d8ff2153d69279ef52c2ae2666acf34
Felipe Fonseca Santos <fonseca.santos.felipe@escola.pr.gov.br>
2025-09-01 09:09:18 -0300

    Add files via upload

---
+++ exer3.js
@@ -0,0 +1,11 @@
+const idade = 17
+
+function podeDirigir(idade){
+if (idade >= 18) 
+     {console.log ("Você pode tirar carteira de motorista!");
+} else { console.log ("Você ainda nao tem idade para dirigir");
+} 
+}
+
+
+podeDirigir(idade);
